34d55fb6e897987c671c947cca624f7096b5e309
Jonas Fagerberg <info@csharpschool.com>
2023-09-28 12:07:05 +0200

    Linq & Lambda

---
+++ CollectionsAndExceptions/Classes/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace CollectionsAndExceptions.Classes;
+
+public class Card
+{
+    public int Value { get; set; }
+    public Suits Suit { get; set; }
+
+    public Card(int value, Suits suit) => (Value, Suit) = (value, suit);
+}
---
+++ CollectionsAndExceptions/Classes/Deck.cs
@@ -0,0 +1,44 @@
+﻿namespace CollectionsAndExceptions.Classes;
+
+public enum Suits
+{
+    Hearts, Spades, Diamonds, Clubs
+}
+
+public class Deck
+{
+    private List<Card> _cards = new();
+    public List<Card> Cards { get; init; } = new();
+
+    /*public Deck()
+    {
+        CreateDeck();
+    }*/
+
+    /*public void Filter(string suit)
+    {
+        Cards.Clear();
+        Cards.AddRange(_cards.Where(c => c.Suit == suit).ToList());
+    }*/
+    public void Filter(Suits suit)
+    {
+        Cards.Clear();
+        Cards.AddRange(_cards.Where(c => c.Suit == suit).ToList());
+    }
+
+    public void CreateDeck()
+    {
+        try
+        {
+            foreach (var suit in Enum.GetValues<Suits>())
+                for (int i = 2; i <= 14; i++)
+                    _cards.Add(new Card(i, suit));
+            
+            Cards.AddRange(_cards);
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
